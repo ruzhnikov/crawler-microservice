6a2c0aceff0979415a3cfcd3ed5b732c6131cb3d
Alexander Ruzhnikov <ruzhnikov85@gmail.com>
2018-05-01 19:40:01 +0300

    * worked variant

--- README.md
+++ README.md
@@ -5,3 +5,23 @@ Exness crowler microservice
 ## DESCRIPTION
 
 Test exercise. Service for obtaining data
+
+## INSTALL
+
+```
+    virtualenv --python=python3 env
+    source ./env/bin/activate
+    pip install Flask
+    cd <path of exness-crawler>
+    python setup.py install
+    cd <path of this app>
+    python app.py
+```
+## USING
+
+Open in your browser a link http://localhost:5000/api/1.0/rss?url=<url>&count=<count>
+Where
+* &lt;url&gt; -- Mandatory argument. A whole address of RSS resource, for example https://habr.com/rss/best/
+* &lt;count&gt; -- Optional argument. A count of records that you want to get. By default, unlimited
+
+The response of server will be containt JSON with crabbed data
---
+++ app.py
@@ -0,0 +1,81 @@
+
+# -*- coding: utf-8 -*-
+
+
+import json
+import flask
+from exness.crawler import Crawler
+from exness.rss import RssLoad
+
+
+ITEM_MANDATORY_FIELDS = ("title", "link", "published", "description")
+UNLIMITED_ITEMS_COUNT = -1
+
+
+app = flask.Flask(__name__)
+
+def collect_feed(feed_data):
+    def local_data(feed):
+        feed_data["title"] = feed.title
+    
+    return local_data
+
+def collect_items(items_data, items_count=UNLIMITED_ITEMS_COUNT):
+    def local_data(item):
+        len_items_data = len(items_data)
+        # check limit of items. If there is limit of data and we already have
+        # enough items, just skip next step
+        if int(items_count) > 0 and int(len_items_data) >= int(items_count):
+            return False
+
+        local_item = dict()
+        for key in ITEM_MANDATORY_FIELDS:
+            local_item[key] = item.get(key)
+
+        items_data.append(local_item)
+
+    return local_data
+
+def get_rss_data(url, count):
+    feed_data = dict()
+    items_data = list()
+
+    feed_handler = collect_feed(feed_data)
+    item_handler = collect_items(items_data, count)
+
+    rss_object = RssLoad(feed_handler, item_handler)
+    crawler = Crawler(rss_object)
+
+    res = crawler.process(url)
+    if res.success:
+        return flask.Response(
+            status=200,
+            mimetype="application/json",
+            response=json.dumps((feed_data, items_data)) + "\n"
+        )
+    else:
+        return flask.Response(
+            status=500,
+            response=res.error
+        )
+
+
+@app.route('/')
+def root():
+    return flask.redirect('/api/1.0/rss')
+
+@app.route("/api/1.0/rss", methods=["GET"])
+def rss():
+    url = flask.request.args.get("url")
+    if url is None:
+        return flask.Response(status=500, response="Give me an URL")
+
+    count = flask.request.args.get("count")
+    if count is None:
+        count = UNLIMITED_ITEMS_COUNT
+
+    return get_rss_data(url, count)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
